e224c5e46daf15d0c1ca0147054d5aadd2e1d5d9
alesta88 <38480965+alesta88@users.noreply.github.com>
2020-05-14 11:30:44 +0900

    xxx2

--- Assets/Plugins/MoonIsland/Util/Extension/UniRxExtensions.cs
+++ Assets/Plugins/MoonIsland/Util/Extension/UniRxExtensions.cs
@@ -9,7 +9,7 @@ public static class UniRxExtensions {
     /// <summary>
     /// Observableボタンを作成
     /// </summary>
-    static public UniRx.IObservable<PointerEventData> ObservableButton( this GameObject btnObject, Component controller ) {
+    static public IObservable<PointerEventData> ObservableButton( this GameObject btnObject, Component controller ) {
         var btnInputArea = btnObject.GetComponent<ButtonInputArea>();
         GameObject btnObj = btnInputArea != null ? btnInputArea.Area.gameObject : btnObject.gameObject;
 
@@ -22,7 +22,7 @@ public static class UniRxExtensions {
     /// <summary>
     /// Observableボタンを作成
     /// </summary>//alesta unirx
-    static public UniRx.IObservable<PointerEventData> ObservableButton( this Image btnImage, Component controller ) {
+    static public IObservable<PointerEventData> ObservableButton( this Image btnImage, Component controller ) {
         return ObservableButton( btnImage.gameObject, controller );
     }
 }
